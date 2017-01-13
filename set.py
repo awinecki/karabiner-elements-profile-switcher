73918e142bd895fa89710bbe279ea2a3ee343003
awinecki <winecki@gmail.com>
2017-01-13 03:05:20 +0100

    Initial commit; workflow code

---
+++ set.py
@@ -0,0 +1,18 @@
+import sys
+import json
+from os.path import expanduser
+
+CHOSEN_PROFILE = sys.argv[1]
+CONFIG_PATH = '.config/karabiner/karabiner.json'
+
+
+home = expanduser("~")
+config = {}
+
+with open('{}/{}'.format(home, CONFIG_PATH)) as conf_file:
+    config = json.load(conf_file)
+    for profile in config['profiles']:
+        profile['selected'] = profile['name'] == CHOSEN_PROFILE
+
+with open('{}/{}'.format(home, CONFIG_PATH), 'w') as conf_file:
+    conf_file.write(json.dumps(config))
