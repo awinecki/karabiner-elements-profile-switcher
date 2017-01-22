312b2e127f71c5be25f080db96baab906c8ced4b
awinecki <winecki@gmail.com>
2017-01-22 02:03:27 +0100

    Keep keys order in karabiner.json file

--- set.py
+++ set.py
@@ -1,6 +1,7 @@
 import sys
 import json
 from os.path import expanduser
+from collections import OrderedDict
 
 CHOSEN_PROFILE = sys.argv[1]
 CONFIG_PATH = '.config/karabiner/karabiner.json'
@@ -10,7 +11,7 @@ home = expanduser("~")
 config = {}
 
 with open('{}/{}'.format(home, CONFIG_PATH)) as conf_file:
-    config = json.load(conf_file)
+    config = json.load(conf_file, object_pairs_hook=OrderedDict)
     for profile in config['profiles']:
         profile['selected'] = profile['name'] == CHOSEN_PROFILE
 
